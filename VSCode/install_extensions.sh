c2c511d86e3366e3e6507acb5fe818d2f59cea53
Ofri Gabso <ofri@entrio.io>
2024-06-18 16:52:01 +0300

    Add vscode extensions installation script

---
+++ VSCode/install_extensions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-python.black-formatter
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.isort
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-toolsai.vscode-jupyter-cell-tags
+code --install-extension ms-toolsai.vscode-jupyter-slideshow
+code --install-extension rangav.vscode-thunder-client
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension vscodevim.vim
+code --install-extension yoavbls.pretty-ts-errors
